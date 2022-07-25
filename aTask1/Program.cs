5fee33bc3f2aa6482cd79feb6238ae91afabcc43
Fray <kozar20000@mail.ru>
2022-07-25 16:10:35 +0300

    descendingRows

---
+++ aTask1/Program.cs
@@ -0,0 +1,56 @@
+﻿Console.WriteLine($"\nВведите число строк  массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [,] array = new int[m, n];
+
+
+void GetArray (int m, int n)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 11);
+
+        }
+    }
+}
+
+void PrintArray (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.Write("\n");
+    }
+}
+
+void DescendingRows (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }        
+    }
+}
+
+GetArray(3, 3);
+PrintArray (array);
+Console.WriteLine("Результат:");
+DescendingRows (array);
+PrintArray (array);
